ad4554b2107fe056b12f8e8034c9305af3a01628
marcocar007 <marcocarlosavalos@gmail.com>
2025-07-30 11:13:34 -0600

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="py-3 text-center text-xs border-t bg-white">
+      Aplicación creada por el maestro Marco Carlos Avalos Rosado —{' '}
+      <a href="https://www.ucem.edu.mx/" target="_blank" rel="noopener noreferrer" className="underline text-blue-600">https://www.ucem.edu.mx/</a>
+    </footer>
+  );
+}
